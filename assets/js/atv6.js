4ab4e4b1d0168ec4c53e45c35c6e888eb3ce2a36
Thiago Silva Soares <tsoares.dev@gmail.com>
2023-01-25 11:25:20 -0300

    update atv

--- assets/js/atv6.js
+++ assets/js/atv6.js
@@ -1,12 +1,12 @@
-const imprimeArray = () => {
-  let quantidades = document.getElementById('campo1').value;
+const printArray = () => {
+  let quantidade = document.getElementById('campo1').value;
   let valorMin = document.getElementById('campo2').value;
   let valorMax = document.getElementById('campo3').value;
 
-  if(valorMin < valorMax && (valorMax-valorMin) >= quantidades){
+  if(valorMin < valorMax && (valorMax-valorMin) >= quantidade){
   let recebe = [valorMin,valorMax];
 
-  for(var i = 1; i <= quantidades-2; i++){
+  for(var i = 1; i <= quantidade-2; i++){
   recebe.push(Math.round(Math.random() * (valorMax - valorMin) + Number(valorMin)));
   }
   let ordem = recebe.sort((a,b) => {
@@ -17,6 +17,6 @@ const imprimeArray = () => {
   document.getElementById('resultado').innerHTML = ordem
   } else{
   let result1 = document.getElementById('resultado').innerHTML
-  document.getElementById('resultado').innerHTML = "Valores invalidos!"
+  document.getElementById('resultado').innerHTML = "Valores invalidos"
   }
 }
